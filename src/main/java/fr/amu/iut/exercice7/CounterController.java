61fa4631ab40d65a2189a108242f0d693f3b7b09
Maxence <maxence.torchin@etu.univ-amu.fr>
2025-05-05 10:14:21 +0200

    Complétion de l'exercice 7

--- src/main/java/fr/amu/iut/exercice7/CounterController.java
+++ src/main/java/fr/amu/iut/exercice7/CounterController.java
@@ -1,18 +1,28 @@
 package fr.amu.iut.exercice7;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CounterController implements Initializable {
 
+    @FXML
+    private Label counterLabel;
+
     int counter = 0;
 
     public void increment() {
+        counter++;
+        counterLabel.setText(String.valueOf(counter));
     }
 
     public void decrement() {
+        counter--;
+        counterLabel.setText(String.valueOf(counter));
     }
 
     @Override
