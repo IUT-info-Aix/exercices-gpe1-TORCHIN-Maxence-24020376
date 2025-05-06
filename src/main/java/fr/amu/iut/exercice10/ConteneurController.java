9d8f2560f61938fdab5c0a31db3fa2976cb8ea36
Maxence <maxence.torchin@etu.univ-amu.fr>
2025-05-06 16:56:13 +0200

    Exercice 10 et finalisation du TP 3

---
+++ src/main/java/fr/amu/iut/exercice10/ConteneurController.java
@@ -0,0 +1,57 @@
+package fr.amu.iut.exercice10;
+
+import com.example.partie3.AlertHelper;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
+
+public class ConteneurController {
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        Window owner = submitButton.getScene().getWindow();
+        if(nameField.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter your name");
+            return;
+        }
+        if(emailField.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter your email id");
+            return;
+        }
+        if(passwordField.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter a password");
+            return;
+        }
+
+        AlertHelper.showAlert(Alert.AlertType.CONFIRMATION, owner, "Registration Successful!",
+                "Welcome " + nameField.getText());
+    }
+
+    @FXML
+    protected void handleCancelButtonAction(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation");
+        alert.setHeaderText(null);
+        alert.setContentText("Vous voulez quitter le formulaire?");
+        alert.showAndWait();
+    }
+
+}
